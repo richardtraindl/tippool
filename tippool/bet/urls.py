f5b9334a0490bb4c944a29bc7be4058abc0c7638
richardtraindl <richardtraindl@gmail.com>
2014-06-25 20:34:03 +0200

    changes for mybets

--- tippool/bet/urls.py
+++ tippool/bet/urls.py
@@ -5,10 +5,13 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('bet.views',
     url(r'^games/$', 'games'),
     url(r'^games/(?P<eventid>\w+)/$', 'games'),
+    url(r'^mybets/$', 'mybets'),
+    url(r'^mybets/(?P<eventid>\w+)/$', 'mybets'),
     url(r'^bets/$', 'bets'),
     url(r'^bets/(?P<poolid>\w+)/$', 'bets'),
     url(r'^bets/(?P<poolid>\w+)/(?P<eventid>\w+)/$', 'bets'),
     url(r'^add_bet/$', 'add_bet'),
+    url(r'^add_bet/(?P<betid>\w+)/$', 'add_bet'),
     url(r'^add_bet/(?P<poolid>\w+)/(?P<eventid>\w+)/(?P<betid>\w+)/$', 'add_bet'),
     url(r'^ranking/$', 'ranking'),
 )
--- tippool/bet/views.py
+++ tippool/bet/views.py
@@ -19,6 +19,53 @@ class MatchBets(object):
             self.betlist.append(bet)
 
 
+def mybets(request, eventid=1):
+    context = RequestContext(request)
+
+    # all pools where user is member
+    pools = Pool.objects.filter(membership__user__id=request.user.id, active=True)
+
+    # event
+    if eventid == None:
+        event = Event.objects.get(poolevent__pool_id=pools[0].id, active=True)
+    else:
+        event = Event.objects.get(id=eventid)
+
+
+    # all events for choosen pool
+    events = Event.objects.filter(poolevent__pool__membership__user_id=request.user.id, active=True)
+
+    # all matches for choosen event
+    matches = Match.objects.all().filter(event_id=event.id)
+
+    # all bets for choosen pool and event
+    match_bets = []
+    for match in matches:
+        match_bet = MatchBets(match)
+
+        for pool in pools:
+            try:
+                membership = Membership.objects.get(pool_id=pool.id, user_id=request.user.id)
+                account= Account.objects.get(event_id=match.event_id, membership_id=membership.id)
+                bet = Bet.objects.get(match_id=match.id, account_id=account.id)
+                match_bet.betlist.append(bet)
+            except Bet.DoesNotExist:
+                bet = Bet()
+                bet.account_id = account.id
+                bet.match_id = match.id
+                bet.overtime = False
+                bet.penalties = False
+                bet.team1_score_regular = 0
+                bet.team2_score_regular = 0
+                bet.save()
+                match_bet.betlist.append(bet)
+                continue
+
+        match_bets.append(match_bet)
+
+    return render(request, 'bet/mybets.html', {'body_id': 'mybets', 'events': events, 'event': event,  'match_bets': match_bets } )
+
+
 
 def bets(request, poolid=1, eventid=1):
     context = RequestContext(request)
@@ -57,8 +104,6 @@ def bets(request, poolid=1, eventid=1):
             
         match_bets.append(match_bet)
 
-        
-
     return render(request, 'bet/bets.html', {'body_id': 'bets', 'pool': pool, 'pools': pools, 'event': event, 'events': events, 'match_bets': match_bets, 'user': request.user } )
 
 
@@ -86,7 +131,7 @@ def games(request, eventid=1):
 
 
 
-def add_bet(request, poolid=1, matchid=1):
+def add_bet2(request, poolid=1, matchid=1):
     # Get the context from the request.
     context = RequestContext(request)
 
@@ -147,3 +192,37 @@ def add_bet(request, poolid=1, matchid=1):
             bet.save()
 
         return render_to_response('bet/add_bet.html', {'form': form, 'body_id': 'add_bet', 'bet': bet, 'match': match, 'pool': pool }, context)
+
+
+def add_bet(request, betid=1):
+    # Get the context from the request.
+    context = RequestContext(request)
+
+    # A HTTP POST?
+    if request.method == 'POST':
+        bet_id = request.POST['bet']
+        match_id = request.POST['match']
+
+        bet = Bet.objects.get(id=bet_id)
+        match = Match.objects.get(id=match_id)
+
+        form = BetForm(request.POST, instance=bet)
+        if form.is_valid():
+            form.save(commit=True)
+            return HttpResponseRedirect("/bet/mybets/" + str(match.event_id))
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print form.errors
+            return HttpResponse(form.errors)
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = BetForm()
+
+        if betid == None:
+            return HttpResponse(form.errors)
+        else:
+            bet = Bet.objects.get(id=betid)
+
+        match = Match.objects.get(id=bet.match_id)
+
+        return render_to_response('bet/add_bet.html', {'form': form, 'body_id': 'add_bet', 'bet': bet, 'match': match }, context)
